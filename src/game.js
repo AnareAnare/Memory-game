138b7db8d70b6128f9e4fe075565a5032bf2dcd3
AnareAnare <122323178+AnareAnare@users.noreply.github.com>
2023-05-26 22:36:53 +0300

    + gsap

--- src/game.js
+++ src/game.js
@@ -8,9 +8,9 @@
       [array[i], array[j]] = [array[j], array[i]];
     }
   }
-  let arr1 = [];
-  let arr2 = [];
   function mainArray(numberOfCards) {
+    let arr1 = [];
+    let arr2 = [];
     numberOfCards *= numberOfCards;
     numberOfPairs = numberOfCards / 2;
     for (let i = 1; i <= numberOfPairs; i++) {
@@ -33,20 +33,64 @@
       setTimeout(() => input.classList.remove('wrong-input'), 2000);
     }
 
-    function endOfTheGame() {
-      if (document.querySelectorAll('.success-style').length == numbersArray.length) {
-        alert('Победа! Принимайте поздравления!');
+    function afterAnimation() {
+      central.innerHTML = '';
+      const endButton = document.createElement('button');
+      endButton.classList.add('start-button');
+      endButton.textContent = 'Сыграть ещё раз';
+      central.append(endButton);
+      endButton.addEventListener('click', () => {
         central.innerHTML = '';
-        const endButton = document.createElement('button');
-        endButton.classList.add('start-button');
-        endButton.textContent = 'Сыграть ещё раз';
-        central.append(endButton);
-        endButton.addEventListener('click', () => {
-          central.innerHTML = '';
-          central.classList.remove('central-added');
-          initialDiv.classList.remove('vanish');
-          central.append(initialDiv);
+        central.classList.remove('central-added');
+        initialDiv.classList.remove('vanish');
+        central.append(initialDiv);
+        numbersArray = [];
+      });
+      alert('Победа! Принимайте поздравления!');
+    }
+
+    function delayedAfterAnimation() {
+      setTimeout(afterAnimation, 2000);
+    }
+
+    function endOfTheGame() {
+      const cardsAllSuccess = document.querySelectorAll('.success-style');
+
+      function animationWinner() {
+        const tl = gsap.timeline({ onComplete: delayedAfterAnimation });
+        tl.to('.success-style', {
+          keyframes: {
+            '0%': { scale: 1 },
+            '50%': { scale: 1.3, ease: 'power1.out' },
+            '75%': { scale: 0.9 },
+            '100%': { scale: 1.2 },
+          },
+          duration: 1,
+          stagger: {
+            amount: 0.5,
+            grid: "auto",
+            from: "center"
+          }
         });
+        tl.to('.success-style', {
+          rotate: "random(50, 270)",
+          stagger: {
+            grid: "auto",
+            from: "center",
+            ease: "power3.out"
+          }
+        });
+        tl.to('.success-style', {
+          y: "random(100, 500)",
+          stagger: {
+            grid: "auto",
+            from: "center",
+            ease: "power3.out"
+          }
+        }, '<');
+      }
+      if (cardsAllSuccess.length == numbersArray.length) {
+        animationWinner();
       }
     }
     button.addEventListener('click', () => {
@@ -55,10 +99,24 @@
         initialDiv.classList.add('vanish');
         central.classList.add('central-added');
         createMapField(+input.value);
+        startAnimation('.animation-style');
       } else {
         wrongInput();
       }
     });
+
+    function startAnimation(el) {
+      let tl = gsap.timeline({ onComplete: removeTransform });
+      tl.to(el, {y: 0, opacity: 1, stagger: {from: 0, amount: 0.5}});
+      function removeTransform() {
+        document.querySelectorAll('.shirt-style').forEach((item) => {
+          item.classList.remove('animation-style');
+          item.style.removeProperty('transform');
+        });
+      }
+    }
+
+    
     function createMapField(a) {
       let firstCard = null;
       let secondCard = null;
@@ -66,11 +124,12 @@
       const cardWidth = Math.floor(100 / a) - 1 + '%';
       for (let i = 0; i < cards; i++) {
         let card = document.createElement('div');
-        card.classList.add('shirt-style');
+        card.classList.add('shirt-style', 'animation-style');
         card.style.width = cardWidth;
         card.style.height = cardWidth;
         card.textContent = numbersArray[i];
         central.append(card);
+        
         card.addEventListener('click', () => {
           card.classList.add('card-style');
           if (firstCard !== null && secondCard !== null) {
@@ -96,7 +155,7 @@
               secondCard = null;
             }
           }
-          setTimeout(endOfTheGame, 1000);
+          endOfTheGame();
         });
       }
     }
